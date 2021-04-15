bb29ac14daadca80f559f3f738106b4da15cd7f8
Alan Renner <renner.131@osu.edu>
2021-04-14 18:49:14 -0700

    Finish with part two

--- TICTACTOE.js
+++ TICTACTOE.js
@@ -14,9 +14,39 @@ var board = {
   9: " ",
 };
 
-//Function that places a mark onto the board
+//Function that marks the board at the correct position
 function markBoard(position, mark) {
-  board[position] = mark.toUpperCase();
+  board[position] = mark;
+}
+
+//function that prints the current tic tac toe board
+function printBoard() {
+  console.log(
+    "\n" +
+      " " +
+      board[1] +
+      " | " +
+      board[2] +
+      " | " +
+      board[3] +
+      "\n" +
+      " ---------\n" +
+      " " +
+      board[4] +
+      " | " +
+      board[5] +
+      " | " +
+      board[6] +
+      "\n" +
+      " ---------\n" +
+      " " +
+      board[7] +
+      " | " +
+      board[8] +
+      " | " +
+      board[9] +
+      "\n"
+  );
 }
 
 //Create a game loop that prompts each player to make a move, takes a command line argument, and prints it back
@@ -24,7 +54,8 @@ function TICTACTOE(player) {
   console.log("Your turn player: " + player);
   prompt.start();
   prompt.get(["position"], function (err, result) {
-    console.log(`Player  ${player} + placed at  ${result.position}`);
+    markBoard(result.position, player);
+    printBoard();
   });
 }
 
