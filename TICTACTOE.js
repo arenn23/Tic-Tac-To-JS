253fc86debd023db2edee3b26007a1cf96b976a6
Alan Renner <renner.131@osu.edu>
2021-04-14 18:29:47 -0700

    initial board created

--- TICTACTOE.js
+++ TICTACTOE.js
@@ -1,2 +1,15 @@
-var msg = "hello world";
-console.log(msg);
+//Node and prompt are the only two dependencies needed for this game. Run commands: npm i node npm install prompt
+var prompt = require("prompt");
+
+//Blank intitial board object
+var board = {
+  1: " ",
+  2: " ",
+  3: " ",
+  4: " ",
+  5: " ",
+  6: " ",
+  7: " ",
+  8: " ",
+  9: " ",
+};
